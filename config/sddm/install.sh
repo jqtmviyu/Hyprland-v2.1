3fb925162e636ee47c83d6858364db6226176326
jqtmviyu <jqtmviyu@gmail.com>
2024-07-31 21:47:55 +0800

    fix: :bug: sddm install err

--- config/sddm/install.sh
+++ config/sddm/install.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 yay -S --noconfirm sddm-git
-sudo mkdir -p /ssdm-macos /usr/share/sddm/themes
-sudo cp -a ./ssdm-macos /usr/share/sddm/themes/
+sudo mkdir -p /usr/share/sddm/themes/ssdm-macos
+sudo cp -a ./ssdm-macos/* /usr/share/sddm/themes/
 sudo mkir -p /etc/sddm.conf.d
 sudo cp ./10-theme.conf /etc/sddm.conf.d/
 sudo systemctl restart sddm
