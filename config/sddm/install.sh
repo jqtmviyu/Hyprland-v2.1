79fddf0c1f10e8cb17d7fbcee48e9904066334c3
jqtmviyu <jqtmviyu@gmail.com>
2024-10-07 00:47:40 +0800

    fix: :bug: sddm-macos theme install shell spell error

--- config/sddm/install.sh
+++ config/sddm/install.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 yay -S --noconfirm sddm-git
-sudo mkdir -p /usr/share/sddm/themes/ssdm-macos
-sudo cp -a ./ssdm-macos/* /usr/share/sddm/themes/
+sudo mkdir -p /usr/share/sddm/themes
+sudo cp -a ./sddm-macos /usr/share/sddm/themes/
 sudo mkir -p /etc/sddm.conf.d
 sudo cp ./10-theme.conf /etc/sddm.conf.d/
 sudo systemctl restart sddm
