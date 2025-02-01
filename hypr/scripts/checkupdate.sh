84bd0d7d39508f980987e0172b49ed9c84e16ecb
jqtmviyu <jqtmviyu@gmail.com>
2025-02-01 14:08:42 +0800

    update checkupdatte script

--- hypr/scripts/checkupdate.sh
+++ hypr/scripts/checkupdate.sh
@@ -2,13 +2,12 @@
 
 if [[ "$1" == "clean" ]]; then
   echo "input sudo pass to clean"
-  sudo pacman -Rs $(pacman -Qdtq)
+  sudo pacman -Rns $(pacman -Qdtq)
   sudo pacman -Sc
-  yay -Scc
+	sudo paccache -r
+	rm -rf ~/.cache/yay
   sudo journalctl --vacuum-time=1month
   sudo journalctl --vacuum-size=500M
-  rm -rf ~/.cache/*
-  sudo rm -rf /tmp/*
 else
   num=$(checkupdates | wc -l)
   echo '{"text": '"$num"', "alt": "", "tooltip": "L:update\nR:clean"}'
